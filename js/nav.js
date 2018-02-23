cb3975db42ca838ab9beb68a247b19173cda7613
Everett Bradshaw <mrtonybradshaw@gmail.com>
2018-02-23 14:02:21 -0600

    CSS Updates

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,12 @@
 class Nav {
     static init() {
-        this.navBtn  = document.querySelector('.nav-btn-icon');
+        this.navBtn  = document.querySelector('.hamburger');
         this._addEventListeners();
     }
 
     static _addEventListeners() {
     	this.navBtn.addEventListener('click', e => {
       		document.body.classList.toggle('nav-in');
-
-     		if ( e.target.textContent === 'Open' ) {
-    			e.target.textContent = 'Close';
-    		}
-    		else {
-    			e.target.textContent = 'Open';
-    		}
     	});
     }
 }
